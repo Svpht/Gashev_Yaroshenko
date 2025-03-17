b00c3373db4ed154f05cff9efe14b51713c093a4
Svpht <svyatoslav.gashev@bk.ru>
2025-03-17 17:14:44 +0500

    it worked!

---
+++ basa.sql
@@ -0,0 +1,28 @@
+CREATE SCHEMA IF NOT EXISTS gashev_yaroshenko;
+CREATE TABLE IF NOT EXISTS gashev_yaroshenko.taxi_cars (
+    id_car INTEGER PRIMARY KEY,
+    model TEXT NOT NULL,
+    color TEXT NOT NULL,
+    car_number TEXT UNIQUE NOT NULL,
+    free_status BOOL NOT NULL
+);
+CREATE TABLE IF NOT EXISTS gashev_yaroshenko.driver (
+    driver_id INTEGER PRIMARY KEY,
+    rating_drive INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS gashev_yaroshenko.passag (
+    user_id INTEGER PRIMARY KEY,
+    rating INTEGER NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS gashev_yaroshenko.orders (
+    order_id INTEGER PRIMARY KEY,
+    FOREIGN KEY (id_car) REFERENCES gashev_yaroshenko.taxi_cars(id_car),
+    FOREIGN KEY (user_id) REFERENCES gashev_yaroshenko.passag(user_id),
+    FOREIGN KEY (driver_id) REFERENCES gashev_yaroshenko.driver(driver_id),
+    point_from TEXT NOT NULL,
+    point_to TEXT NOT NULL,
+    time_of_order TEXT NOT NULL,
+    status TEXT NOT NULL
+);
---
+++ main.py
@@ -0,0 +1,23 @@
+import psycopg2
+from psycopg2 import sql
+DB_HOST = '79.174.88.238'
+DB_PORT = 15221
+DB_NAME = 'school_db'
+DB_USER = 'school'
+DB_PASSWORD = 'School1234*'
+
+connect = psycopg2.connect(
+    dbname=DB_NAME,
+    host = DB_HOST,
+    port = DB_PORT,
+    user = DB_USER,
+    password = DB_PASSWORD
+)
+
+bebr=connect.cursor()
+print("соединение с бд успешно")
+with open("basa.sql", "r") as file:
+    script=file.read()
+bebr.execute(script)
+connect.commit()
+print("таблица успешно сохранена")
